852920ede7ea32f3f745e254eee069eb28638b21
softprops <d.tangren@gmail.com>
2024-06-30 22:11:55 -0400

    refactor example

--- examples/demo/main.zig
+++ examples/demo/main.zig
@@ -14,7 +14,30 @@ pub fn main() !void {
             .{ "hello", RawBson.string("world") },
         },
     );
+    const bytes = try serialize(allocator, doc);
+    defer allocator.free(bytes);
+    std.debug.print("{s}", .{std.fmt.fmtSliceHexLower(bytes)});
 
+    // read it back
+    var rawBson = try deserialize(allocator, bytes);
+    defer rawBson.deinit();
+    switch (rawBson.value) {
+        .document => |v| {
+            if (v.get("hello")) |value| {
+                std.debug.print("deserialized hello '{s}'!", .{value});
+            }
+        },
+        else => unreachable,
+    }
+}
+
+fn deserialize(allocator: std.mem.Allocator, bytes: []const u8) !bson.Owned(RawBson) {
+    var fbs = std.io.fixedBufferStream(bytes);
+    var reader = bson.reader(allocator, fbs.reader());
+    return try reader.read();
+}
+
+fn serialize(allocator: std.mem.Allocator, doc: RawBson) ![]const u8 {
     // write a document to a byte buffer
     var buf = std.ArrayList(u8).init(allocator);
     defer buf.deinit();
@@ -25,29 +48,5 @@ pub fn main() !void {
     defer writer.deinit();
     try writer.write(doc);
 
-    const bytes = try buf.toOwnedSlice();
-    defer allocator.free(bytes);
-    std.debug.print(
-        "{s}",
-        .{
-            std.fmt.fmtSliceHexLower(bytes),
-        },
-    );
-
-    // read it back
-    var fbs = std.io.fixedBufferStream(bytes);
-    var reader = bson.reader(allocator, fbs.reader());
-    var rawBson = try reader.read();
-    defer rawBson.deinit();
-    switch (rawBson.value) {
-        .document => |v| {
-            if (v.get("hello")) |value| {
-                std.debug.print(
-                    "deserialized hello '{s}'!",
-                    .{value},
-                );
-            }
-        },
-        else => unreachable,
-    }
+    return try buf.toOwnedSlice();
 }
